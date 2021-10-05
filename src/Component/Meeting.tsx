5fd083b4be19bd0b011c9ee2db824e36d3bbdaa3
SantGT5 <gianspf@gmail.com>
2021-10-05 02:12:17 +0200

    GianLucas - Meeting Date

--- src/Component/Meeting.tsx
+++ src/Component/Meeting.tsx
@@ -1,11 +1,22 @@
+import { ArrowBackIosRounded } from "@mui/icons-material";
 import React, { useState } from "react";
+import { Input } from "../Global/Input";
 
 interface dateUpdate {
   now: "" | {};
 }
 
+interface inputArr {
+  name: string;
+  timer: number;
+}
+
 export const Meeting = () => {
   const [date, setDate] = useState<dateUpdate>({ now: "" });
+
+  const [arr, setArr] = useState<inputArr[] | any>([]);
+
+  console.log(arr);
 
   function display_ct6() {
     const x = new Date();
@@ -27,10 +38,37 @@ export const Meeting = () => {
   }
   setTimeout(display_ct6, 1000);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setArr({ ...arr, [event.currentTarget.name]: event.currentTarget.value });
+  };
+
   return (
     <div>
-      {date.now ? <h4 className="date">{date.now}</h4 > : <div className="loader"></div>}
+      {date.now ? (
+        <h4 className="date">{date.now}</h4>
+      ) : (
+        <div className="loader"></div>
+      )}
       <h4 className="date">test</h4>
+
+      <Input
+        label="name"
+        type="text"
+        value={arr.name}
+        name="name"
+        className="margin form-control"
+        autocomplete="off"
+        onChange={handleChange}
+      />
+      <Input
+        label="Tiempo"
+        type="number"
+        value={arr.timer}
+        name="timer"
+        className="margin form-control"
+        autocomplete="off"
+        onChange={handleChange}
+      />
     </div>
   );
 };
